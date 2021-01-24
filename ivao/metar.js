9809d5211fdd9623978aa2c7d2c0b0dfd21a5901
Alexcaussades <alexcaussades@gmail.com>
2021-01-24 18:35:24 +0100

    fix metar 2

--- ivao/metar.js
+++ ivao/metar.js
@@ -1,15 +1,9 @@
-const fetch = require("node-fetch")
 const request = require("request")
 module.exports.run = (client, channel, tags, message, self, args) => {
     const configivao = require("./api.json")
-    // fetch(configivao.data.metar+args,{
-    //     method: "GET"
-    // }).then((body) =>{
-    //     console.log(body.body)
-    // })
-
     request(configivao.data.metar+args, function (error, response, body) {
-       console.log(body)
+       //console.log(body)
+       client.say(channel, "Metar information for " + body)
       });
     
 }
