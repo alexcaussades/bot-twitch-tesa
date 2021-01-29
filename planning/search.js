a277920ad249ff323406f2c9becc77e96fc0dca8
Alexcaussades <alexcaussades@gmail.com>
2021-01-29 23:48:12 +0100

    set jour de la semaine v2

--- planning/search.js
+++ planning/search.js
@@ -1,14 +1,14 @@
 const authtwitch = require("../twitch.json");
 const debug = require("../twitch_modules/debug.json");
 const fetch = require("node-fetch");
-const { Client } = require("tmi.js");
+//const { Client, client } = require("tmi.js");
 
-module.exports.run = () => {
+module.exports.run = (client, channel , pdo) => {
     let debut = new Date()
-    console.log(debut.getDay())
     let i = debut.getDay()
     const jours = [[1, "lundi"], [2, "mardi"], [3, "mercredi"], [4, "jeudi"], [5, "vendredi"], [6, "samedi"], [7, "dimanche"]]
     const day = new Map(jours)
-    console.log(day.get(i))
-    
+    const jour = day.get(i)
+    const heure = debut.getHours()
+    client.say(channel, `The planning from the API {{'Status' : '200', 'method': 'POST'}} at the days : ${jour} for time: ${heure}` )
 }
